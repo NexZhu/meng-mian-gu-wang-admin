c84b3ae3e501e86c4d08b1ddde315c0d0324d16d
Nex Zhu <nexzhu@gmail.com>
2016-06-05 14:44:29 +0800

    Add Jubao model and controller

---
+++ api/controllers/JubaoController.js
@@ -0,0 +1,11 @@
+/**
+ * JubaoController
+ *
+ * @description :: Server-side logic for managing jubaos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Jubao.js
@@ -0,0 +1,28 @@
+/**
+ * Jubao.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: '举报内容',
+  attributes: {
+    comment: {
+      model: 'comment',
+      columnName: '评论_ID',
+    },
+    user: {
+      model: 'user',
+      columnName: '举报人',
+    },
+    time: {
+      type: 'datetime',
+      columnName: '创建时间',
+    },
+    content: {
+      type: 'string',
+      columnName: '内容',
+    },
+  },
+}
